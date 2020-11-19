4f84d1ee9e4cfd9287aa9590a37f09007f51565f
Marko Idzan <42409619+idzan@users.noreply.github.com>
2020-11-19 14:49:06 +0100

    added navigation
    finally working
    html: basic layout with wcag 2.0 specification
    css: flex and basic styling, vanilla code
    js: basic code for opening and closing menu

--- js/code.js
+++ js/code.js
@@ -1,1 +1,8 @@
-// Demo Code
+// Navigation
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+
+navToggle.addEventListener('click', () => {
+    nav.classList.toggle('nav--visible');
+})
