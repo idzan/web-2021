4dfe61bfe3f0b1fb255443f5aa2c0c2e2ba9b13e
Marko Idzan <42409619+idzan@users.noreply.github.com>
2021-01-26 18:27:31 +0100

    added to top button
    one more item finished, fuck yeah

--- js/code.js
+++ js/code.js
@@ -69,3 +69,30 @@ modeToggleButton.addEventListener('click', evt => {
 });
 
 applySetting();
+
+// To Top Button
+const scrollToTopButton = document.getElementById('to-top');
+
+const scrollFunction = () => {
+    let y = window.scrollY;
+    if (y > 0) {
+        scrollToTopButton.className = "top show";
+    } else {
+        scrollToTopButton.className = "top hide";
+    }
+};
+
+window.addEventListener("scroll", scrollFunction);
+
+const scrollToTop = () => {
+    const c = document.documentElement.scrollTop || document.body.scrollTop;
+    if (c > 0) {
+        window.requestAnimationFrame(scrollToTop);
+        window.scrollTo(0, c - c / 5);
+    }
+};
+
+scrollToTopButton.onclick = function(e) {
+    e.preventDefault();
+    scrollToTop();
+}
